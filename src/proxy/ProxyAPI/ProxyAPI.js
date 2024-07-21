a115ceae78eaa15f6fbb52c82d04c6a9b24f2afa
Nikolai Petukhov <n.petukhov@innopolis.university>
2024-07-21 20:10:26 +0300

    update link

--- src/proxy/ProxyAPI/ProxyAPI.js
+++ src/proxy/ProxyAPI/ProxyAPI.js
@@ -4,7 +4,7 @@ import {RequestType} from "../Request/RequestType.tsx";
 import requestToServer from "../Request/RequestToServer";
 
 // const URL = 'http://127.0.0.1:8000/';
-const URL = 'https://792b-2a0e-d602-1-ef4-00-2.ngrok-free.app/'
+const URL = 'https://9f99-188-130-155-182.ngrok-free.app/'
 const API = ''
 
 export default class ProxyAPI{
